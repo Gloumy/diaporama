8982b33d5bc108e6e80aac4c3fdd56e596c7fe71
Gloumy <simon@cornet.dev>
2020-02-26 16:12:57 +0100

    base posts_state

--- lib/states/posts_state.dart
+++ lib/states/posts_state.dart
@@ -1,8 +1,16 @@
 import 'package:diaporama/models/post.dart';
+import 'package:diaporama/repositories/posts_repository.dart';
 import 'package:flutter/material.dart';
 
 class PostsState with ChangeNotifier {
   List<Post> _posts = [];
 
   List<Post> get posts => List.from(_posts);
+
+  PostsRepository _postsRepository = PostsRepository();
+
+  Future<void> retrievePosts(String subreddit) async {
+    _posts = await _postsRepository.retrievePosts(subreddit);
+    notifyListeners();
+  }
 }
