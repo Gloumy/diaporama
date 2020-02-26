382dca9a25ea1d6e9091028d55b66bee3844bbb7
Simon Cornet <simon@creative-atoms.be>
2020-02-26 15:01:55 +0100

    flutter create

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:diaporama/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Diaporama',
+      home: HomeScreen(),
+    );
+  }
+}
