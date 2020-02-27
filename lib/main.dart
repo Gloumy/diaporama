9d172fab249aa8f182426f1ad5eb1cd6cf405263
Gloumy <simon@cornet.dev>
2020-02-27 11:00:48 +0100

    init reddit client, set auth url and launch it

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,25 @@
 import 'package:diaporama/screens/home_screen.dart';
 import 'package:diaporama/states/posts_state.dart';
+import 'package:diaporama/utils/secrets.dart';
+import 'package:draw/draw.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final reddit = Reddit.createInstalledFlowInstance(
+    clientId: redditSecret,
+    userAgent: "diaporama-app",
+    redirectUri: Uri.parse("diaporama://cornet.dev"),
+  );
+  final authUrl = reddit.auth.url(['*'], "diaporama");
+
+  launch(authUrl.toString());
+
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
