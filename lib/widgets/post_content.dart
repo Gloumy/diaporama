e03671833c14853c83f364538231a9fd325497fb
Gloumy <simon@cornet.dev>
2020-03-02 16:25:35 +0100

    implement "gifvideo" content

--- lib/widgets/post_content.dart
+++ lib/widgets/post_content.dart
@@ -3,6 +3,7 @@ import 'package:draw/draw.dart';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:video_player/video_player.dart';
+import 'package:video_provider/video_provider.dart';
 
 class PostContent extends StatefulWidget {
   final Submission post;
@@ -24,6 +25,11 @@ class _PostContentState extends State<PostContent> {
     super.initState();
     if (getPostType() == PostType.Video) {
       _playerController = VideoPlayerController.network(_post.url.toString());
+      _initializeVideoPlayerFuture = _playerController.initialize();
+    }
+    if (getPostType() == PostType.GifVideo) {
+      Uri videoUrl = VideoProvider.fromUri(_post.url).getVideos().first.uri;
+      _playerController = VideoPlayerController.network(videoUrl.toString());
       _initializeVideoPlayerFuture = _playerController.initialize();
     }
   }
@@ -62,6 +68,33 @@ class _PostContentState extends State<PostContent> {
                 aspectRatio: _playerController.value.aspectRatio,
                 // Use the VideoPlayer widget to display the video.
                 child: VideoPlayer(_playerController),
+              );
+            } else {
+              // If the VideoPlayerController is still initializing, show a
+              // loading spinner.
+              return Center(child: CircularProgressIndicator());
+            }
+          },
+        );
+        break;
+      case PostType.GifVideo:
+        widget = FutureBuilder(
+          future: _initializeVideoPlayerFuture,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              // If the VideoPlayerController has finished initialization, use
+              // the data it provides to limit the aspect ratio of the VideoPlayer.
+              return Column(
+                children: <Widget>[
+                  AspectRatio(
+                    aspectRatio: _playerController.value.aspectRatio,
+                    // Use the VideoPlayer widget to display the video.
+                    child: VideoPlayer(_playerController),
+                  ),
+                  RaisedButton(
+                      onPressed: () => _playerController.play(),
+                      child: Icon(Icons.play_arrow)),
+                ],
               );
             } else {
               // If the VideoPlayerController is still initializing, show a
