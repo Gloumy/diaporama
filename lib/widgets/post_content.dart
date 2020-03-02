ca8406276a185158bd02ed8368eecf90e95f2a28
Gloumy <simon@cornet.dev>
2020-03-02 16:06:24 +0100

    start to implement video player

--- lib/widgets/post_content.dart
+++ lib/widgets/post_content.dart
@@ -2,23 +2,44 @@ import 'package:diaporama/models/post_type.dart';
 import 'package:draw/draw.dart';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:video_player/video_player.dart';
 
-class PostContent extends StatelessWidget {
+class PostContent extends StatefulWidget {
   final Submission post;
 
   const PostContent({Key key, this.post}) : super(key: key);
 
+  @override
+  _PostContentState createState() => _PostContentState();
+}
+
+class _PostContentState extends State<PostContent> {
+  Submission get _post => widget.post;
+
+  VideoPlayerController _playerController;
+  Future<void> _initializeVideoPlayerFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    if (getPostType() == PostType.Video) {
+      _playerController = VideoPlayerController.network(_post.url.toString());
+      _initializeVideoPlayerFuture = _playerController.initialize();
+    }
+  }
+
   PostType getPostType() {
-    if (post.isSelf) return PostType.SelfPost;
-    if (["i.redd.it", "gfycat.com", "i.imgur.com"].contains(post.domain)) {
-      if (post.url.toString().contains('.gifv') ||
-          post.domain.contains("gfycat.com")) {
+    if (_post.isSelf) return PostType.SelfPost;
+    if (["i.redd.it", "i.imgur.com"].contains(_post.domain)) {
+      if (_post.url.toString().contains('.gifv')) {
         return PostType.GifVideo;
       } else {
         return PostType.Image;
       }
     }
-    if (post.isVideo) return PostType.Video;
+    if (["v.redd.it", "gfycat.com"].contains(_post.domain))
+      return PostType.GifVideo;
+    if (_post.isVideo) return PostType.Video;
 
     return PostType.Link;
   }
@@ -26,23 +47,39 @@ class PostContent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Widget widget;
-
     switch (getPostType()) {
       case PostType.SelfPost:
-        widget = Text(post.selftext);
+        widget = Text(_post.selftext);
         break;
       case PostType.Video:
-        widget = Text("Video");
+        widget = FutureBuilder(
+          future: _initializeVideoPlayerFuture,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              // If the VideoPlayerController has finished initialization, use
+              // the data it provides to limit the aspect ratio of the VideoPlayer.
+              return AspectRatio(
+                aspectRatio: _playerController.value.aspectRatio,
+                // Use the VideoPlayer widget to display the video.
+                child: VideoPlayer(_playerController),
+              );
+            } else {
+              // If the VideoPlayerController is still initializing, show a
+              // loading spinner.
+              return Center(child: CircularProgressIndicator());
+            }
+          },
+        );
         break;
       case PostType.Image:
-        widget = Image.network(post.url.toString());
+        widget = Image.network(_post.url.toString());
         break;
       case PostType.Link:
         widget = Column(
           children: <Widget>[
-            Text(post.url.toString()),
+            Text(_post.url.toString()),
             RaisedButton(
-              onPressed: () => launch(post.url.toString()),
+              onPressed: () => launch(_post.url.toString()),
               child: Text("Go to URL"),
             ),
           ],
@@ -56,5 +93,11 @@ class PostContent extends StatelessWidget {
     }
 
     return widget;
+  }
+
+  @override
+  void dispose() {
+    _playerController?.dispose();
+    super.dispose();
   }
 }
