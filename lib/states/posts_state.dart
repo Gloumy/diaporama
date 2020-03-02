01f2fbae33b116644483543eb0d6685e308d0447
Gloumy <simon@cornet.dev>
2020-03-02 09:52:59 +0100

    initialize posts state with reddit client service

--- lib/states/posts_state.dart
+++ lib/states/posts_state.dart
@@ -1,9 +1,14 @@
 import 'package:diaporama/models/post.dart';
 import 'package:diaporama/repositories/posts_repository.dart';
+import 'package:diaporama/services/reddit_client_service.dart';
 import 'package:flutter/material.dart';
 
 class PostsState with ChangeNotifier {
+  final RedditClientService redditService;
+
   List<Post> _posts = [];
+
+  PostsState({@required this.redditService});
 
   List<Post> get posts =>
       List.from(_posts.where((p) => p.domain == "i.redd.it"));
