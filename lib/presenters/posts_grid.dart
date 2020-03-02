097ab78ceaf9c155a0cc0582b277698a0667b61a
Simon Cornet <simon@creative-atoms.be>
2020-03-02 11:33:00 +0100

    display posts previews in posts grid

--- lib/presenters/posts_grid.dart
+++ lib/presenters/posts_grid.dart
@@ -1,30 +1,33 @@
 import 'package:diaporama/models/post.dart';
 import 'package:diaporama/screens/viewer_screen.dart';
 import 'package:diaporama/states/posts_state.dart';
+import 'package:diaporama/widgets/media_thumbnail.dart';
+import 'package:draw/draw.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 class PostsGrid extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Expanded(child: Consumer<PostsState>(
-      builder: (context, state, _) {
-        return GridView.count(
-          shrinkWrap: true,
-          crossAxisCount: 3,
-          children: <Widget>[
-            for (Post post in state.posts)
-              GestureDetector(
-                onTap: () => Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => ViewerScreen())),
-                child: Image.network(
-                  post.thumbnailUrl,
-                  fit: BoxFit.cover,
-                ),
-              ),
-          ],
-        );
-      },
-    ));
+    return Expanded(
+      child: Consumer<PostsState>(
+        builder: (context, state, _) {
+          return GridView.builder(
+            gridDelegate:
+                SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
+            shrinkWrap: true,
+            itemCount: state.contents.length,
+            itemBuilder: (context, index) {
+              Submission post = state.contents[index];
+              return post.isSelf
+                  ? Text(post.title)
+                  : MediaThumbnail(
+                      post: post,
+                    );
+            },
+          );
+        },
+      ),
+    );
   }
 }
