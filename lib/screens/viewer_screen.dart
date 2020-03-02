05a4f7a9b2ef4cbefeac34b08506c3d0ca3c3358
Gloumy <simon@cornet.dev>
2020-03-02 12:09:09 +0100

    pass starting index to viewer screen

--- lib/screens/viewer_screen.dart
+++ lib/screens/viewer_screen.dart
@@ -3,17 +3,32 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 class ViewerScreen extends StatefulWidget {
+  final int startingIndex;
+
+  const ViewerScreen({Key key, this.startingIndex}) : super(key: key);
+
   @override
   _ViewerScreenState createState() => _ViewerScreenState();
 }
 
 class _ViewerScreenState extends State<ViewerScreen> {
+  PageController _controller;
+
+  @override
+  void initState() {
+    _controller = PageController(
+      initialPage: widget.startingIndex,
+    );
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(child: Consumer<PostsState>(
         builder: (context, state, _) {
           return PageView.builder(
+              controller: _controller,
               itemCount: state.contents.length,
               itemBuilder: (context, index) => Container(
                     height: double.maxFinite,
