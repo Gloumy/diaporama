3ec89d16ee55744983cb195fd8b324f7e9906c68
Gloumy <simon@cornet.dev>
2020-03-02 08:52:15 +0100

    dark theme colors

---
+++ lib/utils/base_theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:diaporama/utils/colors.dart';
+
+final ThemeData darkTheme = ThemeData(
+    scaffoldBackgroundColor: mediumGreyColor,
+    appBarTheme: AppBarTheme(
+      color: darkGreyColor,
+    ));
---
+++ lib/utils/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const Color darkGreyColor = Color(0xFF303841);
+const Color mediumGreyColor = Color(0xFF3a4750);
+const Color lightGreyColor = Color(0xFFeeeeee);
+const Color blueColor = Color(0xFF00adb5);
+const Color redditOrange = Color(0xFFFF5700);
