24099dbfe82c17e5225f10fe27c4bd1b37dead15
Simon Cornet <simon@cornet.dev>
2020-03-14 16:41:53 +0100

    auth button if username is null

--- lib/presenters/redditor/user_greeting.dart
+++ lib/presenters/redditor/user_greeting.dart
@@ -1,7 +1,10 @@
 import 'package:diaporama/states/global_state.dart';
 import 'package:diaporama/utils/colors.dart';
+import 'package:diaporama/utils/secrets.dart';
+import 'package:draw/draw.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class UserGreeting extends StatelessWidget {
   @override
@@ -17,6 +20,20 @@ class UserGreeting extends StatelessWidget {
                 color: lightGreyColor,
               ),
             ),
+            if (username == null)
+              IconButton(
+                  icon: Icon(
+                    Icons.person,
+                    color: redditOrange,
+                  ),
+                  onPressed: () {
+                    Reddit reddit = Reddit.createInstalledFlowInstance(
+                      clientId: redditSecret,
+                      userAgent: "diaporama-app",
+                      redirectUri: Uri.parse("diaporama://cornet.dev"),
+                    );
+                    launch(reddit.auth.url(["*"], "diaporama-auth").toString());
+                  }),
           ],
         );
       },
