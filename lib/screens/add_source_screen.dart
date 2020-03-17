442729175a3ae19ec4ff363d2fe793cac9fd8184
Simon <simon@cornet.dev>
2020-03-17 09:07:38 +0100

    navigate to add source screen

---
+++ lib/screens/add_source_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AddSourceScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text("add source")),
+    );
+  }
+}
--- lib/widgets/add_new_source_card.dart
+++ lib/widgets/add_new_source_card.dart
@@ -1,26 +1,12 @@
-import 'package:diaporama/utils/colors.dart';
-import 'package:diaporama/widgets/new_source_choice_sheet.dart';
+import 'package:diaporama/screens/add_source_screen.dart';
 import 'package:flutter/material.dart';
 
-class AddNewSourceCard extends StatefulWidget {
-  @override
-  _AddNewSourceCardState createState() => _AddNewSourceCardState();
-}
-
-class _AddNewSourceCardState extends State<AddNewSourceCard> {
-  void _displayNewSourceModal() {
-    showModalBottomSheet(
-        context: context,
-        backgroundColor: darkGreyColor,
-        builder: (context) {
-          return NewSourceChoiceSheet();
-        });
-  }
-
+class AddNewSourceCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: _displayNewSourceModal,
+      onTap: () => Navigator.push(
+          context, MaterialPageRoute(builder: (context) => AddSourceScreen())),
       child: Card(
         child: Center(child: Text("+")),
       ),
