f2176192d16783aaf6c75a564e2bda2d281b2ba8
Simon <simon@cornet.dev>
2020-03-21 15:38:41 +0100

    style add_source inputs

--- lib/screens/add_source_screen.dart
+++ lib/screens/add_source_screen.dart
@@ -35,81 +35,152 @@ class _AddSourceScreenState extends State<AddSourceScreen> {
             ),
             onPressed: () => Navigator.pop(context)),
       ),
-      body: SingleChildScrollView(
-        padding: EdgeInsets.symmetric(
-          vertical: 15,
-          horizontal: 25,
-        ),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              TextFormField(
-                decoration: InputDecoration(labelText: "Source name"),
-                controller: _nameController,
-                validator: (value) {
-                  if (value.isEmpty) return "Required";
-                  return null;
-                },
-              ),
-              SizedBox(
-                height: 15.0,
-              ),
-              TypeAheadField(
-                  textFieldConfiguration: TextFieldConfiguration(
+      body: GestureDetector(
+        onTap: () => FocusScope.of(context).requestFocus(FocusNode()),
+        child: Container(
+          height: double.maxFinite,
+          width: double.maxFinite,
+          child: SingleChildScrollView(
+            padding: EdgeInsets.symmetric(
+              vertical: 15,
+              horizontal: 25,
+            ),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  TextFormField(
                     decoration: InputDecoration(
-                        labelText: "Subreddits",
-                        border: OutlineInputBorder(
-                            borderSide: BorderSide(
+                      labelText: "Source name",
+                      labelStyle: TextStyle(
+                        color: lightGreyColor,
+                      ),
+                      hintStyle: TextStyle(
+                        color: redditOrange,
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
                           color: blueColor,
-                        )),
-                        suffixIcon: Icon(Icons.search),
-                        prefixIcon: GestureDetector(
-                          child: Icon(Icons.cancel),
-                          onTap: () => _searchController.clear(),
-                        )),
-                    controller: _searchController,
-                  ),
-                  suggestionsCallback: (value) async =>
-                      await _searchListener(value),
-                  itemBuilder: (context, value) {
-                    return Text(value);
-                  },
-                  onSuggestionSelected: (value) {
-                    setState(() {
-                      _subreddits.add(value);
-                    });
-                  }),
-              SizedBox(
-                height: 5,
-              ),
-              Text(
-                "Selected subreddits",
-                style: TextStyle(fontSize: 18),
-              ),
-              if (_subreddits.isEmpty)
-                Text(
-                  "Select at least one subreddit",
-                  style: TextStyle(color: Colors.red),
-                ),
-              ListView(
-                shrinkWrap: true,
-                children: [
-                  for (String sub in _subreddits)
-                    ListTile(
-                      title: Text(sub),
-                      leading: IconButton(
-                          icon: Icon(Icons.remove_circle),
-                          onPressed: () {
-                            setState(() {
-                              _subreddits.remove(sub);
-                            });
-                          }),
+                        ),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: redditOrange,
+                          width: 2.0,
+                        ),
+                      ),
                     ),
+                    controller: _nameController,
+                    validator: (value) {
+                      if (value.isEmpty) return "Required";
+                      return null;
+                    },
+                  ),
+                  SizedBox(
+                    height: 15.0,
+                  ),
+                  TypeAheadField(
+                    textFieldConfiguration: TextFieldConfiguration(
+                      decoration: InputDecoration(
+                          labelText: "Subreddits",
+                          labelStyle: TextStyle(
+                            color: lightGreyColor,
+                          ),
+                          hintStyle: TextStyle(
+                            color: redditOrange,
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: blueColor,
+                            ),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: redditOrange,
+                              width: 2.0,
+                            ),
+                          ),
+                          suffixIcon: Icon(
+                            Icons.search,
+                            color: blueColor,
+                          ),
+                          prefixIcon: GestureDetector(
+                            child: Icon(
+                              Icons.cancel,
+                              color: blueColor,
+                            ),
+                            onTap: () => _searchController.clear(),
+                          )),
+                      controller: _searchController,
+                    ),
+                    suggestionsCallback: (value) async =>
+                        await _searchListener(value),
+                    itemBuilder: (context, value) {
+                      return Container(
+                        child: Text(
+                          value,
+                          style: TextStyle(color: lightGreyColor),
+                        ),
+                        height: 25,
+                        margin: EdgeInsets.symmetric(
+                          horizontal: 10,
+                          vertical: 10,
+                        ),
+                      );
+                    },
+                    onSuggestionSelected: (value) {
+                      setState(() {
+                        _subreddits.add(value);
+                      });
+                    },
+                    suggestionsBoxDecoration: SuggestionsBoxDecoration(
+                      color: darkGreyColor,
+                    ),
+                    noItemsFoundBuilder: (context) {
+                      return Container(
+                        margin: EdgeInsets.symmetric(
+                          horizontal: 15.0,
+                          vertical: 10.0,
+                        ),
+                        child: Text(
+                          "No subreddits found !",
+                          style: TextStyle(color: lightGreyColor),
+                        ),
+                      );
+                    },
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Text(
+                    "Selected subreddits",
+                    style: TextStyle(fontSize: 18),
+                  ),
+                  if (_subreddits.isEmpty)
+                    Text(
+                      "Select at least one subreddit",
+                      style: TextStyle(color: Colors.red),
+                    ),
+                  ListView(
+                    shrinkWrap: true,
+                    children: [
+                      for (String sub in _subreddits)
+                        ListTile(
+                          title: Text(sub),
+                          leading: IconButton(
+                              icon: Icon(Icons.remove_circle),
+                              onPressed: () {
+                                setState(() {
+                                  _subreddits.remove(sub);
+                                });
+                              }),
+                        ),
+                    ],
+                  )
                 ],
-              )
-            ],
+              ),
+            ),
           ),
         ),
       ),
