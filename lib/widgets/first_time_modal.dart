53fa5964ba49d853da4ef688f3a1882092052ad2
Simon <simon@cornet.dev>
2020-03-25 17:14:06 +0100

    styling for first time modal

--- lib/widgets/first_time_modal.dart
+++ lib/widgets/first_time_modal.dart
@@ -1,3 +1,4 @@
+import 'package:diaporama/utils/colors.dart';
 import 'package:diaporama/utils/secrets.dart';
 import 'package:draw/draw.dart';
 import 'package:flutter/material.dart';
@@ -7,23 +8,52 @@ class FirstTimeModal extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
-      title: Text("Welcome to Diaporama"),
-      content: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          Text("Hello there (General Kenobi) !"),
-          Text(
-              "To get full functionalites from Diaporama (no limit on posts count, access to subreddit search and more), you need to connect to your Reddit account."),
-          Text(
-              "If you don't, no worries, you'll still be able to use it, but you'll be limited in the content."),
-          Text("So, you gonna connect or not ?"),
-        ],
+      title: Text(
+        "Welcome to Diaporama",
+        style: TextStyle(
+          color: redditOrange,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      backgroundColor: mediumGreyColor,
+      content: Container(
+        padding: EdgeInsets.all(10),
+        margin: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: darkGreyColor,
+          borderRadius: BorderRadius.circular(5),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Text(
+              "Hello there (General Kenobi) ! \n",
+              style: TextStyle(color: lightGreyColor),
+            ),
+            Text(
+              "To get a more personnalized experience, you need to connect to your Reddit account. \n",
+              style: TextStyle(color: lightGreyColor),
+            ),
+            Text(
+              "If you don't, that's cool, you'll still be able to use it, but you might be limited in the content. \n",
+              style: TextStyle(color: lightGreyColor),
+            ),
+            Text(
+              "So, you gonna connect or what ?",
+              style: TextStyle(color: lightGreyColor),
+            ),
+          ],
+        ),
       ),
       actions: <Widget>[
         FlatButton(
-            onPressed: () => Navigator.pop(context), child: Text("Nope.")),
+          onPressed: () => Navigator.pop(context),
+          child: Text("Nope."),
+          textColor: Colors.red,
+        ),
         RaisedButton(
+            color: blueColor,
             onPressed: () {
               Reddit reddit = Reddit.createInstalledFlowInstance(
                 clientId: redditSecret,
