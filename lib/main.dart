18aa5029d2f42305de426b84d00bed305b195ee8
Simon <simon@cornet.dev>
2020-03-30 11:06:10 +0200

    add initialization to main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,58 @@
+import 'dart:async';
+
 import 'package:diaporama/screens/home_screen.dart';
 import 'package:diaporama/services/hive_service.dart';
 import 'package:diaporama/states/global_state.dart';
 import 'package:diaporama/states/posts_state.dart';
 import 'package:diaporama/states/subreddits_state.dart';
 import 'package:diaporama/utils/base_theme.dart';
+import 'package:diaporama/utils/secrets.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:sentry/sentry.dart';
 import 'package:uni_links/uni_links.dart';
 
-void main() async {
+final SentryClient _sentry = new SentryClient(dsn: sentryDSN);
+
+/// Reports [error] along with its [stackTrace] to Sentry.io.
+Future<Null> _reportError(dynamic error, dynamic stackTrace) async {
+  print('Caught error: $error');
+
+  // Errors thrown in development mode are unlikely to be interesting. You can
+  // check if you are running in dev mode using an assertion and omit sending
+  // the report.
+  if (isInDebugMode) {
+    print(stackTrace);
+    print('In dev mode. Not sending report to Sentry.io.');
+    return;
+  }
+
+  print('Reporting to Sentry.io...');
+
+  final SentryResponse response = await _sentry.captureException(
+    exception: error,
+    stackTrace: stackTrace,
+  );
+
+  if (response.isSuccessful) {
+    print('Success! Event ID: ${response.eventId}');
+  } else {
+    print('Failed to report to Sentry.io: ${response.error}');
+  }
+}
+
+Future<Null> main() async {
+  FlutterError.onError = (FlutterErrorDetails details) async {
+    if (isInDebugMode) {
+      // In development mode simply print to console.
+      FlutterError.dumpErrorToConsole(details);
+    } else {
+      // In production mode report to the application zone to report to
+      // Sentry.
+      Zone.current.handleUncaughtError(details.exception, details.stack);
+    }
+  };
+
   WidgetsFlutterBinding.ensureInitialized();
 
   GlobalState globalState = GlobalState();
@@ -27,9 +71,13 @@ void main() async {
     throw (e);
   }
 
-  runApp(MyApp(
-    globalState: globalState,
-  ));
+  runZoned<Future<Null>>(() async {
+    runApp(MyApp(
+      globalState: globalState,
+    ));
+  }, onError: (error, stackTrace) async {
+    await _reportError(error, stackTrace);
+  });
 }
 
 class MyApp extends StatelessWidget {
