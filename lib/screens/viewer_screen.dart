aeafe59b9d738685c18d9103de36866bf49a3234
Simon <simon@cornet.dev>
2020-04-06 10:35:48 +0200

    ask confirmation for leaving viewer screen

--- lib/screens/viewer_screen.dart
+++ lib/screens/viewer_screen.dart
@@ -1,4 +1,5 @@
 import 'package:diaporama/states/posts_state.dart';
+import 'package:diaporama/utils/colors.dart';
 import 'package:diaporama/widgets/post_content.dart';
 import 'package:diaporama/widgets/post_header.dart';
 import 'package:draw/draw.dart';
@@ -25,30 +26,83 @@ class _ViewerScreenState extends State<ViewerScreen> {
     super.initState();
   }
 
+  Future<bool> _exitView(BuildContext context) async {
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text(
+              "Exit ?",
+              style: TextStyle(
+                color: redditOrange,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            backgroundColor: mediumGreyColor,
+            content: Container(
+              padding: EdgeInsets.all(10),
+              margin: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                color: darkGreyColor,
+                borderRadius: BorderRadius.circular(5),
+              ),
+              child: Text(
+                "Are you sure you want to go back ? Your progression in threads will be reset.",
+                style: TextStyle(
+                  color: lightGreyColor,
+                ),
+              ),
+            ),
+            actions: <Widget>[
+              FlatButton(
+                onPressed: () => Navigator.of(context).pop(false),
+                child: Text(
+                  "Stay",
+                  style: TextStyle(
+                    color: redditOrange,
+                  ),
+                ),
+              ),
+              RaisedButton(
+                onPressed: () => Navigator.of(context).pop(true),
+                color: blueColor,
+                child: Text(
+                  "Exit",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ],
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(child: Consumer<PostsState>(
         builder: (context, state, _) {
-          return PageView.builder(
-              controller: _controller,
-              itemCount: state.contents.length,
-              itemBuilder: (context, index) {
-                Submission post = state.contents[index];
-                bool loadMore =
-                    (index > state.contents.length - 10 && !state.isLoading);
-                return SingleChildScrollView(
-                  child: Column(
-                    children: <Widget>[
-                      PostHeader(post: post),
-                      PostContent(
-                        post: post,
-                        loadMore: loadMore,
-                      ),
-                    ],
-                  ),
-                );
-              });
+          return WillPopScope(
+            onWillPop: () => _exitView(context),
+            child: PageView.builder(
+                controller: _controller,
+                itemCount: state.contents.length,
+                itemBuilder: (context, index) {
+                  Submission post = state.contents[index];
+                  bool loadMore =
+                      (index > state.contents.length - 10 && !state.isLoading);
+                  return SingleChildScrollView(
+                    child: Column(
+                      children: <Widget>[
+                        PostHeader(post: post),
+                        PostContent(
+                          post: post,
+                          loadMore: loadMore,
+                        ),
+                      ],
+                    ),
+                  );
+                }),
+          );
         },
       )),
     );
